590b164fad52668398f7d5bf721c0365c06640ed
mitchbeebe <beebe.mitch@gmail.com>
2023-09-11 16:21:26 -0400

    Moved to postrgres

--- birdle/settings.py
+++ birdle/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'birdle.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'birdle',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 

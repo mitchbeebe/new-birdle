d6adb31b3a2d8b28a3ac979a2d4101614bb576e9
mitchbeebe <beebe.mitch@gmail.com>
2023-09-11 16:26:53 -0400

    Admin classes

--- birds/admin.py
+++ birds/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from birds.models import Bird, Game, Guess, Image
 
-# Register your models here.
+
+@admin.register(Bird)
+class BirdAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "url"]
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ["id", "date", "bird"]
+
+@admin.register(Guess)
+class GuessAdmin(admin.ModelAdmin):
+    list_display = ["id", "game", "user", "bird"]
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ["id", "username", "date_joined", "last_login"]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ["id", "bird", "url"]

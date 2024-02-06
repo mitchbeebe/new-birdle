d1f156b256487ab885905e0f20c39a246e16cd93
mitchbeebe <beebe.mitch@gmail.com>
2024-02-06 16:59:22 -0500

    Updated observable JS funcs for plotting stats

--- birdle/static/birdle/game_history.js
+++ birdle/static/birdle/game_history.js
@@ -3,7 +3,7 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 import * as Plot from "https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm";
 
-export function game_history(div_id) {
+export function game_history(history, div_id) {
     const results = ["Win", "Loss", "Did not play"]
 
     Date.prototype.addDays = function(days) {
@@ -23,39 +23,42 @@ export function game_history(div_id) {
         return dateArray;
     }; 
 
-    const cal = getDates(new Date("2023-11-01"), new Date());
-
+    //const cal = getDates(new Date("2023-01-01"), new Date());
+    const cal = history;
 
     const cal_plot = Plot.plot({
-        title: "Birdle History",
-        x: {tickFormat: Plot.formatWeekday(), tickSize: 0, anchor: "top"},
+        //x: {tickFormat: Plot.formatWeekday(), tickSize: 0, anchor: "top"},
+        x: {axis: null},
         y: {axis: null},
-        fx: {tickFormat: Plot.formatMonth("en", "short"), anchor: "bottom"},
-        color: {type: "categorical", range: ["lightblue", "orange"], legend: true, label: "Daily Result", domain: ["Win", "Loss"]},
-        marginTop: 40,
+        //fx: {tickFormat: Plot.formatMonth("en", "short"), anchor: "bottom"},
+        color: {type: "categorical", range: ["skyblue", "#FFB000", "#e9ecef"], legend: true, label: "Daily Result", domain: ["Win", "Loss", "Did not play"]},
+        marginTop: 60,
+        padding: 0,
         marks: [
         Plot.cell(cal, {
             x: (d) => (new Date(d.Date)).getUTCDay(),
-            fx: (d) => (new Date(d.Date)).getUTCMonth(),
+            fy: (d) => (new Date(d.Date)).getUTCMonth(),
+            fx: (d) => (new Date(d.Date)).getUTCFullYear(),
             y: (d) => d3.utcWeek.count(d3.utcMonth(new Date(d.Date)), new Date(d.Date)),
             fill: (d) => d.Result,
             text: (d) => (new Date(d.Date)).getUTCDate(),
-            inset: 0.5
+            stroke: (d) => "#6c757d"
         }),
         Plot.text(cal, {
             x: (d) => (new Date(d.Date)).getUTCDay(),
-            fx: (d) => (new Date(d.Date)).getUTCMonth(),
+            fy: (d) => (new Date(d.Date)).getUTCMonth(),
+            fx: (d) => (new Date(d.Date)).getUTCFullYear(),
             y: (d) => d3.utcWeek.count(d3.utcMonth(new Date(d.Date)), new Date(d.Date)),
             text: (d) => (new Date(d.Date)).getUTCDate()
         }),
-        Plot.axisX({anchor: "top", tickFormat: Plot.formatWeekday(), tickSize: 0}),
-        Plot.axisFx({tickFormat: Plot.formatMonth("en", "long"), anchor: "top", tickPadding: 30, fontWeight: "bold"}),
+        Plot.axisX({tickFormat: Plot.formatWeekday("en", "narrow"), tickSize: 0, anchor: "top"}),
+        Plot.axisFx({tickFormat: (d) => d.toLocaleString(undefined, {useGrouping: false}), anchor: "top", fontWeight: "bold", tickPadding: 30}),
+        Plot.axisFy({tickFormat: Plot.formatMonth("en", "short"), anchor: "left", fontWeight: "bold"}),
         Plot.tip(cal, Plot.pointer({
             x: (d) => (new Date(d.Date)).getUTCDay(),
-            fx: (d) => (new Date(d.Date)).getUTCMonth(),
+            fy: (d) => (new Date(d.Date)).getUTCMonth(),
             y: (d) => d3.utcWeek.count(d3.utcMonth(new Date(d.Date)), new Date(d.Date)),
-            title: (d) => `${d.Date}: ${d.Result}`,
-            info: "test"
+            title: (d) => `${d.Date}: ${d.Result}`
         }))
         ]
     });
--- birdle/static/birdle/guess_distribution.js
+++ birdle/static/birdle/guess_distribution.js
@@ -4,12 +4,12 @@ import * as Plot from "https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm"
 
 export function guess_distribution(data, div_id) {
     const plot = Plot.plot({
-        title: "Win Distribution",
+        height: 300,
         marks: [
             Plot.barX(data, {x: "count", y: "guesses"}),
-            Plot.text(data, {x: "count", y: "guesses", text: (d) => (d.count), dx: -10, fill: "white", fontSize: 14}),
-            Plot.axisY({label: "Guess Count", fontSize: 14, labelAnchor: "top", labelArrow:"down"}),
-            Plot.axisX({label: "Games Won", ticks: 0, fontSize: 14, labelAnchor: "left"}),
+            Plot.text(data, {x: "count", y: "guesses", text: (d) => (d.count), dx: -10, fill: "white", fontSize: 18}),
+            Plot.axisY({label: "Guess Count", fontSize: 18, labelAnchor: "top", labelArrow: "down"}),
+            Plot.axisX({label: "Games Won", ticks: 0, fontSize: 18, labelAnchor: "left"}),
             Plot.ruleX([0])
         ]});
     const div = document.querySelector(div_id);

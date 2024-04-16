0b8a776822cd3451b639c683aa61d63191f48b54
mitchbeebe <beebe.mitch@gmail.com>
2024-04-16 18:37:27 -0400

    Added validation around lat/long for lower 48 observations

--- birdle/management/commands/update_birdregions.py
+++ birdle/management/commands/update_birdregions.py
@@ -23,6 +23,23 @@ class Command(BaseCommand):
             "aut"
         ]
 
+        # Helper function for determining valid observations
+        def is_valid(observation, region_code):
+            # Exclude all exotic (naturalized, provisional, or escapee) species
+            if observation.get('exoticCategory'):
+                return False
+            # Check lat/long of observation for USA Lower 48
+            elif region_code == "lower48":
+                lat = observation.get('lat')
+                lng = observation.get('lng')
+                if lat > 24.5 and lat < 49.5 and lng < -66.5 and lng > -124.5:
+                    return True
+                else:
+                    return False
+            # Otherwise assume it's a valid observation for the region
+            else:
+                return True
+
         for region_code in region_codes:
             self.stdout.write(f'Beginning update for {region_code}')
             start = datetime.now()
@@ -52,7 +69,7 @@ class Command(BaseCommand):
 
                 # Add all non-exotic species to our list
                 # "Exotic" defined here https://support.ebird.org/en/support/solutions/articles/48001218430-exotic-and-introduced-species-in-ebird
-                species_codes += [observation.get('speciesCode') for observation in observations if not observation.get('exoticCategory')]
+                species_codes += [observation.get('speciesCode') for observation in observations if is_valid(observation, region_code)]
 
             new_birds_in_region = Bird.objects.filter(species_code__in=species_codes)
 
